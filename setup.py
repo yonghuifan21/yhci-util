78017cb7120d6ff0758fdf66d1e7eaa2eff9cd06
范国徽 Jack <jackfan1@yonghui.com>
2022-11-04 17:58:18 +0800

    feat: v0.2 提交

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 setup(
     name='yhciutil',  # How you named your package folder (MyLib)
     packages=['yhciutil'],  # Chose the same as "name"
-    version='0.1',  # Start with a small number and increase it with every change you make
+    version='0.2',  # Start with a small number and increase it with every change you make
     license='MIT',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='branch manager util',  # Give a short description about your library
     author='yonghuifan21',  # Type in your name
     author_email='jackfan1@yonghui.com',  # Type in your E-Mail
     url='https://github.com/yonghuifan21/yhci-util.git',  # Provide either the link to your github or to your website
-    download_url='https://github.com/yonghuifan21/yhci-util.git/archive/v_01.tar.gz',  # I explain this later on
-    keywords=['SOME', 'MEANINGFULL', 'KEYWORDS'],  # Keywords that define your package best
+    download_url='https://github.com/yonghuifan21/yhci-util/blob/master/dist/yhciutil-0.2.tar.gz',  # I explain this later on
+    keywords=['yhciutil', 'yhci', 'ciutil'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
         'os',
         'shutil',
